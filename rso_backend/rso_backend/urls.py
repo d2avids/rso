2404fb49dd524c275ed517743d6d30deda7781fa
Viva <sovestyaaaa@gmail.com>
2024-01-25 16:47:20 +0300

    fix documents rso

--- rso_backend/rso_backend/urls.py
+++ rso_backend/rso_backend/urls.py
@@ -43,6 +43,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static('compititions/documents/',
-                      document_root=os.path.join(settings.BASE_DIR,
-                                                 'events/documents'))
