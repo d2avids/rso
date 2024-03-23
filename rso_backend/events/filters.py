427f53c9c2c8cb474a6e4d12f6a7fcf6689c1408
David Saidov <delightxxls@gmail.com>
2024-03-23 17:03:10 +0300

    add scale_or_direction filter

--- rso_backend/events/filters.py
+++ rso_backend/events/filters.py
@@ -1,9 +1,13 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from events.models import Event
 
 
 class EventFilter(filters.FilterSet):
+    scale_or_direction = filters.CharFilter(
+        method='filter_scale_or_direction', label='Масштаб или направление'
+    )
     format_type = filters.CharFilter(
         field_name='format', lookup_expr='icontains', label='Формат'
     )
@@ -22,3 +26,21 @@ class EventFilter(filters.FilterSet):
     class Meta:
         model = Event
         fields = ('format_type', 'direction', 'status', 'scale')
+
+    def filter_scale_or_direction(self, queryset, name, value):
+        print(value)
+        filter_values = value.split('|')
+        print(filter_values)
+        q_objects = Q()
+
+        for filter_value in filter_values:
+            if '=' in filter_value:
+                key, val = filter_value.split('=')
+                if key == 'scale' and val:
+                    print(val)
+                    q_objects |= Q(scale__icontains=val)
+                elif key == 'direction' and val:
+                    print(val)
+                    q_objects |= Q(direction__icontains=val)
+
+        return queryset.filter(q_objects)
